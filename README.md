08d6643f7d324d4a5eaabd3741669c723a6359f1
konolga <olgakon.mail@gmail.com>
2019-05-18 15:23:58 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Modern-website
+Modern responsive wedsite template
+https://konolga.github.io/Modern-website/
---
+++ js/custom.js
@@ -0,0 +1,144 @@
+/*==================================
+            SERVICES
+==================================*/
+$(function () {
+    //animate on scroll
+    new WOW().init();
+});
+
+/*==================================
+            WORK
+==================================*/
+$(function () {
+
+    $("#work").magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
+
+    });
+});
+
+/*==================================
+            TEAM
+==================================*/
+$(function () {
+
+    $("#team-members").owlCarousel({
+        items: 3,
+        autoplay: true,
+        smartspeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+
+            480: {
+                items: 2
+            },
+            768: {
+                items: 3
+            }
+
+        }
+    });
+});
+
+/*==================================
+            TESTIMONIALS
+==================================*/
+$(function () {
+
+    $("#customers-testimonials").owlCarousel({
+        items: 1,
+        autoplay: true,
+        smartspeed: 700,
+        loop: true,
+        autoplayHoverPause: true
+    });
+});
+
+/*==================================
+            STATISTICS
+==================================*/
+$(function () {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 3000
+    });
+
+});
+
+
+/*==================================
+            NAVIGATION
+==================================*/
+$(function () {
+    //on scroll event
+    $(window).scroll(function () {
+        if ($(this).scrollTop() < 50) {
+            //hide navigation bar
+            $("nav").removeClass("res-top-nav");
+            $("#back-to-top").fadeOut();
+        } else {
+            //show navigation bar
+            $("nav").addClass("res-top-nav");
+            $("#back-to-top").fadeIn();
+        }
+    });
+
+});
+
+//smoothscrollig
+$(function () {
+    $("a.nav-link").click(function (event) {
+        event.preventDefault();
+
+        //get /return id like #about, #work, #item etc.
+        let section = $(this).attr("href");
+
+        $('html, body').animate({
+            scrollTop: $(section).offset().top - 67
+        }, 1250, "easeInOutExpo"); //easy jquery
+    });
+});
+//cloase mobile menu on click
+$(function () {
+    $(".navbar-collapse ul li a").on("click touch", function () {
+        $(".navbar-toggler").click();
+    })
+})
+/*==================================
+            CLIENTS
+==================================*/
+$(function () {
+
+    $("#clients-list").owlCarousel({
+        items: 6,
+        autoplay: true,
+        smartspeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+
+            480: {
+                items: 3
+            },
+            768: {
+                items: 5
+            },
+            992: {
+                items: 6
+            }
+
+        }
+
+    });
+});
